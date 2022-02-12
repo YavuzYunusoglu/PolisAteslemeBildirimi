89c3f801731c95e5455fe232e45c072455c786f9
Yavuz Yunusoğlu <54793496+YavuzYunusoglu@users.noreply.github.com>
2022-02-12 14:04:45 +0300

    Add files via upload

---
+++ PoliceNotification/Configuration.cs
@@ -0,0 +1,22 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoliceNotification
+{
+    public class Configuration : IRocketPluginConfiguration
+    {
+        public double cooldownSuresi;
+        public string polisID;
+        public string polisIcon;
+        public void LoadDefaults()
+        {
+            polisIcon = "https://image.flaticon.com/icons/png/512/190/190624.png";
+            cooldownSuresi = 7;
+            polisID = "Polis";
+        }
+    }
+}
